0be9a5cdd3ac6bacc0661ecfc805d19fc41276d6
leejhinkle <leej@leejhinkle.org>
2015-09-10 16:57:53 -0600

    $log
    adding $log stuff

--- code-getting-other-services/Starter/app.js
+++ code-getting-other-services/Starter/app.js
@@ -1,5 +1,10 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function($scope) {
+myApp.controller('mainController', function($scope, $log) {
+    $log.info("Some info");
+    $log.debug("Debug info while coding");
+    $log.log("This is the same as console.log()");
+    $log.error("Def an error");
+    $log.warn("Warning!");
     
 });
