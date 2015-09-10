fede2bdfcd680066444b1b6df4540081d24b30fc
leejhinkle <leej@leejhinkle.org>
2015-09-10 17:33:19 -0600

    services + mini
    finishing services and adding magnification

--- code-getting-other-services/Starter/app.js
+++ code-getting-other-services/Starter/app.js
@@ -1,10 +1,23 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', function($scope, $log) {
+myApp.controller('mainController', function($scope, $log, $filter, $resource) {
     $log.info("Some info");
     $log.debug("Debug info while coding");
     $log.log("This is the same as console.log()");
     $log.error("Def an error");
     $log.warn("Warning!");
     
+    $scope.name = 'John'
+    $scope.formattedName = $filter('uppercase')($scope.name);
+    
+    $log.log($scope.name);
+    $log.info($scope.formattedName);
+    
+    console.log($resource);
+    
 });
+
+var things = [1, '2', function(){alert("Hello")}];
+
+things[2]();
+console.log(things);
