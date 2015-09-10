e471177a305c4687a3fc2794a8cbf0681cce36a5
leejhinkle <leej@leejhinkle.org>
2015-09-10 17:57:33 -0600

    []
    showing what arrays can do to help this work better

--- code-minification/Starter/app.js
+++ code-minification/Starter/app.js
@@ -1,5 +1,13 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function($scope, $log) {
-    
-});
+myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+    $log.log($scope);
+}]);
+
+myApp.controller("mainController",["$scope","$log",function(o,l){l.log(o)}]);
+
+//If i minify the above, it changes the names of the variable and breaks dependency injection
+//Solution comes through angular creators
+//by passing an array to the controller.  Allows the inclusion of strings, so we can 
+//pass them into the variable anmes.  Only have to exist in one place.  The first and second
+//controllers are equivalent, although the second is much better
