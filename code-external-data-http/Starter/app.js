7a5617b872d681a6ee3769808e9a89dce29a9887
leejhinkle <leej@leejhinkle.org>
2015-09-11 11:16:09 -0600

    $http
    $http end

--- code-external-data-http/Starter/app.js
+++ code-external-data-http/Starter/app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
+myApp.controller('mainController', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
 
     $scope.handle = '';
 
@@ -9,7 +9,29 @@ myApp.controller('mainController', ['$scope', '$filter', function ($scope, $filt
     };
 
     $scope.characters = 5;
-
+    
+    $http.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk')
+        .success(function(result){
+            $scope.rules = result;
+            console.log(result);            
+        })
+        .error(function(err){
+            console.log(err, status);
+        });
+    
+    $scope.newRule = '';
+    $scope.addRule = function () {
+        /*$http.post('/url', { newRule : $scope.newRule})
+            .success(function(result){
+                $scope.rules = result;
+                $scope.newRule = '';
+            })    
+            .error(function(data, status){
+                console.log(data);
+            })
+        */
+    };
+/* no need for native JS httprequest code.  Going to do it the angular way
     var rulesrequest = new XMLHttpRequest();
     rulesrequest.onreadystatechange = function () {
         $scope.$apply(function () {
@@ -20,5 +42,5 @@ myApp.controller('mainController', ['$scope', '$filter', function ($scope, $filt
     }
     rulesrequest.open("GET", "http://localhost:54765/api", true);
     rulesrequest.send();
-
+*/
 }]);
