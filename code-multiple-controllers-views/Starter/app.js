d3c2332d7dc1403f84e4439433c0fcb13d032e5d
leejhinkle <leej@leejhinkle.org>
2015-09-11 11:23:51 -0600

    multiple controllers finish
    finishe multiple ontrollers.  Coon

--- code-multiple-controllers-views/Starter/app.js
+++ code-multiple-controllers-views/Starter/app.js
@@ -2,6 +2,10 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', function($scope) {
     
+    $scope.name = "main";
     
-    
+}]);
+
+myApp.controller('secondController', ['$scope', function ($scope) {
+    $scope.name = "second";
 }]);
