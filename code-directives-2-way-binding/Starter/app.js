beabd1d8443981d47a07d65c961394c74a87a730
leejhinkle <leej@leejhinkle.org>
2015-09-10 18:34:00 -0600

    finishing data binding.
    Careful of the function fun of lowerhandle()!

--- code-directives-2-way-binding/Starter/app.js
+++ code-directives-2-way-binding/Starter/app.js
@@ -1,7 +1,11 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
+    $scope.handle = ''; //bound to ng-model="handle"
     
+    $scope.lowerhandle = function () {
+        return $filter('lowercase')($scope.handle); //return is critical
+    };
     
     
 }]);
