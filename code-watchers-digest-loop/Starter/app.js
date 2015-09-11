96253987324112f233560f28e97f20ba248b2e48
leejhinkle <leej@leejhinkle.org>
2015-09-10 20:44:24 -0600

    final lesson on watchers and digest loop
    got it done

--- code-watchers-digest-loop/Starter/app.js
+++ code-watchers-digest-loop/Starter/app.js
@@ -1,6 +1,21 @@
-var tb = document.getElementById("name");
+var myApp = angular.module('myApp', []);
 
-tb.addEventListener("keypress",
-    function(event) {
-        console.log("Pressed!");
+myApp.controller('mainController', ['$scope', '$filter', '$timeout', function ($scope, $filter, $timeout) {
+    $scope.handle = '';
+    $scope.lowerhandle = function () {
+        return $filter('lowercase')($scope.handle);
+    };
+    $scope.$watch('handle', function (newValue, oldValue) {
+        console.info('old', oldValue);
+        console.log('new', newValue);
     });
+    
+    //trying fun new things and getting around the digest loop.
+    //vanilla JS.  WTF?
+    $timeout(function () {
+        $scope.$apply(function () {
+            $scope.handle = 'newTwitterHandle';
+            console.log('Handle changed!');
+        });
+    }, 3000);
+}]);
