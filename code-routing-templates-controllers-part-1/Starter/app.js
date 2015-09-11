a8959072e4c37f0647b463b79a07f1b00892cb51
leejhinkle <leej@leejhinkle.org>
2015-09-11 12:12:06 -0600

    creating of fun files
    it’s getting nuts

--- code-routing-templates-controllers-part-1/Starter/app.js
+++ code-routing-templates-controllers-part-1/Starter/app.js
@@ -1,7 +1,32 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.config(function ($routeProvider) {
+    //lets us specify routes.  Will patch patterns also
+    $routeProvider
+        //home page '/'
+        .when('/',{
+            templateUrl: 'pages/main.html',
+            controller: 'mainController'
+        })
+        //other pages '/text'
+        .when('/second',{
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+});
+
+
+
+myApp.controller('mainController', ['$scope', '$location', '$log', function($scope, $location, $log) {
     
+    //$log.info($location.path());
+    $scope.name = '1';
+    
+}]);
 
+myApp.controller('secondController', ['$scope', '$location', '$log', function($scope, $location, $log) {
+    
+    //$log.info($location.path());
+    $scope.name = '2';
     
 }]);
