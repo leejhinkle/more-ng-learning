e1ca2549dbf80116e3c735591db1822d2bb5ebdc
leejhinkle <leej@leejhinkle.org>
2015-09-10 18:10:48 -0600

    added $timeout
    this is super cool.

--- code-scope-and-interpolation/Starter/app.js
+++ code-scope-and-interpolation/Starter/app.js
@@ -1,6 +1,9 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainController', ['$scope', '$timeout', function ($scope, $timeout) {
     
-    
+    $scope.name = "Lee J";
+    $timeout(function () {
+        $scope.name = "Everybody";
+    }, 3000);
 }]);
