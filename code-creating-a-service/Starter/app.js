0fe526746000471ebab667745789e5ca7dc24d00
leejhinkle <leej@leejhinkle.org>
2015-09-12 13:03:50 -0600

    services
    finished

--- code-creating-a-service/Starter/app.js
+++ code-creating-a-service/Starter/app.js
@@ -21,14 +21,34 @@ myApp.config(function ($routeProvider) {
     
 });
 
-myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+myApp.service('nameService',function(){
+    var self = this;
+    this.name = 'JD';
+    this. nameLength = function(){
+        //use self beause this woul refer to the function we are currently in
+        return self.name.length;
+    };
+});
+
+myApp.controller('mainController', ['$scope', '$log', 'nameService', function($scope, $log, nameService) {
     
-    $scope.name = 'Main';
+    $scope.name = nameService.name;
+    //update name service when necessary
+    $scope.$watch('name', function(){
+        nameService.name = $scope.name;
+    });
+    $log.log(nameService.name);
+    $log.log(nameService.nameLength());
     
 }]);
 
-myApp.controller('secondController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', 'nameService', function($scope, $log, $routeParams, nameService) {
     
+    $scope.name = nameService.name;
+    //update name service when necessary
+    $scope.$watch('name', function(){
+        nameService.name = $scope.name;
+    });
     $scope.num = $routeParams.num || 1;
     
 }]);
